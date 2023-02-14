5b26825ec1ef13b17f5bda30299ec2fe9462ad73
TechUX <connect.world12345@gmail.com>
2023-02-14 22:46:41 +0530

    added database configuration file

---
+++ includes/dbconfig.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("MySQL Connection failed: " . mysqli_connect_error());
+  exit() ;
+}
+?>
